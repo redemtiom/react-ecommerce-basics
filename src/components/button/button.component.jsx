936d0cc8cf3484f66de6b2f08b6bbcf78ea182b6
redemtiom <smar.pre.sky@gmail.com>
2023-09-03 16:44:33 -0600

    added stripe components, added stripe logic, updated button with loading

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,4 +1,4 @@
-import {BaseButton, GoogleSignInButton, InvertedButton} from './button.styles'
+import { BaseButton, GoogleSignInButton, InvertedButton, ButtonSpinner } from './button.styles'
 
 export const BUTTON_TYPE_CLASSES = {
     base: 'base',
@@ -8,20 +8,21 @@ export const BUTTON_TYPE_CLASSES = {
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
     {
-        [BUTTON_TYPE_CLASSES.base] : BaseButton,
+        [BUTTON_TYPE_CLASSES.base]: BaseButton,
         [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
         [BUTTON_TYPE_CLASSES.inverted]: InvertedButton
     }[buttonType]
 )
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
     const CustomButton = getButton(buttonType)
 
     return (
         <CustomButton
+            disabled={isLoading}
             {...otherProps}
         >
-            {children}
+            {isLoading ? <ButtonSpinner /> : children}
         </CustomButton>
     )
 }
